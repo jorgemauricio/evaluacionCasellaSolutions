d8f12bcf9ef15a9045f5e06ebc145415767e5038
Jorge Mauricio <jorge.mauricio@me.com>
2018-02-19 19:38:34 -0600

    generar dataframes de cada estación

--- generarEvaluacion.py
+++ generarEvaluacion.py
@@ -34,14 +34,50 @@ def main():
 		for j in listaDeArchivos:
 			nombreTemporalDelArchivo = "data/{}/{}".format(i,j)
 			dataTemp = pd.read_csv(nombreTemporalDelArchivo)
-			frames.append(dataTemp)
+			dataTemp['PM10_1H'] = dataTemp['PM10_15M']
+			dataTemp['PM10_15M'] = dataTemp['PM10_5M']
+			dataTemp['PM10_5M'] = dataTemp['PM10_1M']
+			dataTemp['PM10_1M'] = dataTemp['PM2P5_1H']
+			dataTemp['PM2P5_1H'] = dataTemp['PM2P5_15M']
+			dataTemp['PM2P5_15M'] = dataTemp['PM2P5_5M']
+			dataTemp['PM2P5_5M'] = dataTemp['PM2P5_1M']
+			dataTemp['PM2P5_1M'] = dataTemp['PM1_1H']
+			dataTemp['PM1_1H'] = dataTemp['PM1_15M']
+			dataTemp['PM1_15M'] = dataTemp['PM1_5M']
+			dataTemp['PM1_5M'] = dataTemp['PM1_1M']
+			dataTemp['PM1_1M'] = dataTemp['Time']
+			dataTemp['Time'] = dataTemp.index
+			dataTemp.index = range(len(dataTemp['Time']))
+			dataTemp2 = dataTemp.filter(['Time', 'PM1_1M', 'PM1_5M', 'PM1_15M', 'PM1_1H', 'PM2P5_1M', 'PM2P5_5M','PM2P5_15M', 'PM2P5_1H', 'PM10_1M', 'PM10_5M', 'PM10_15M', 'PM10_1H'])
+			frames.append(dataTemp2)
+			print("***** {}".format(i))
+			print(dataTemp2.head(20))
 
 		# concatenar información
-		data = pd.concat(frames)
+		data = pd.concat(frames,ignore_index=True)
+		print(i)
+		print(data.head())
 
 		# generar csv
-		nombreTemporalParaGuardarArchivo = 'data/{}/{}.csv'.format(i,i)
+		nombreTemporalParaGuardarArchivo = 'data/{}.csv'.format(i,i)
 		data.to_csv(nombreTemporalParaGuardarArchivo)
+
+		# ciclo para generar gráficas
+		for k in data.columns[2:]:
+			plt.clf()
+			plt.figure(figsize=(80,10))
+			print(data["Time"].head())
+			xValues = data['Time']
+			y = data[k]
+			x = range(len(xValues))
+			plt.plot(x,y)
+			plt.title(k)
+			plt.xlabel('Fecha')
+			plt.ylabel('Valor')
+			plt.xticks(x, xValues, rotation='vertical')
+			nombreTemporalGrafica = "data/{}/{}-{}.png".format(i,i,k)
+			plt.savefig(nombreTemporalGrafica, dpi=300)
+			print(nombreTemporalGrafica)
 
 if __name__ == '__main__':
 	main()
